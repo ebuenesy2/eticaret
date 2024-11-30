2333499544baa84e307e2b1acea82bd4606225f4
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2024-11-30 21:35:29 +0300

    ok

--- resources/views/web/include/footer-copyright.blade.php
+++ resources/views/web/include/footer-copyright.blade.php
@@ -1,6 +1,6 @@
 <p class="footer-copyright font-weight-light text-light">Copyright © 2024 Yıldırımdev Tarafından Yapılmıştır. </p>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<a href="https://api.whatsapp.com/send?phone={{ $DB_HomeSettings->phone }}" class="float" target="_blank">
+<a href="https://api.whatsapp.com/send?phone={{ $DB_HomeSettings->whatsapp }}" class="float" target="_blank">
 <i class="fa fa-whatsapp my-float"></i>
 </a>
