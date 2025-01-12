52d60771617004010f85d54eb778be4409c886d8
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2025-01-12 20:07:04 +0300

    upload

--- resources/views/admin/finance/current_account_extra.blade.php
+++ resources/views/admin/finance/current_account_extra.blade.php
@@ -176,6 +176,7 @@
                                   <td data-cell="@lang('admin.actions')" >
                                     <button class="btn btn-success" title="clone" id="cloneItem" data_id="{{$dbFind[$i]->id}}" ><i data_id="{{$dbFind[$i]->id}}" class=" icon-copy"></i></button>
                                     <button class="btn btn-primary" title="modal edit"  id="editItem" href="#editModal" data-toggle="modal" data_id="{{$dbFind[$i]->id}}" ><i data_id="{{$dbFind[$i]->id}}" class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-warning" title="modal fileupload"  id="editFileUpload" href="#editFileUploadModal" data-toggle="modal" data_id="{{$dbFind[$i]->id}}" ><i data_id="{{$dbFind[$i]->id}}" class="fa fa-file"></i></button>
                                     <button class="btn btn-danger" id="deleteItem" data_id="{{$dbFind[$i]->id}}"><i data_id="{{$dbFind[$i]->id}}" class="fa fa-trash "></i></button>
                                   </td>
                               </tr>
@@ -495,6 +496,96 @@
       </div>
   </div>
   <!----  Modal Güncelle Son -->
+        
+  <!----  Modal Dosya Yükleme -->
+  <div id="editFileUploadModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="editFileUploadModalLabel" aria-hidden="true">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="editFileUploadModalTitle" style="display: flex;" ><p> Dosya Yükleme #</p><p id="editFileUploadModalValueId">54</p> </h3>
+        <div id='loaderEditFileUpload' style="display: flex;width: 20px;"><img src="/upload/images/loader.gif" alt=""></div>
+      </div>
+      <div class="modal-body">
+
+        <div class="row-fluid">
+          <div class="span12">
+              <div class="control-group">
+                  <label class="control-label">Dosya Yükle</label>
+                  <div class="controls controls-row">
+
+                      <!-- Dosya Yükleme Kutusu ----->
+                      <div style="border: 2px solid;padding: 10px;">
+
+                          <!-- Dosya Yükleme ----->
+                          <form method="POST" id="uploadForm" enctype="multipart/form-data">
+                              <div style="display: flex;flex-direction: column; gap: 15px;">
+
+                                  <!-- Dosya Yükleme Bilgileri ----->
+                                  <input type="hidden" name="fileDbSave" id="fileDbSave" value="true" >
+                                  <input type="hidden" name="fileWhere" id="fileWhere" value="Sabit" >
+
+                                  <!---  Loading --->
+                                  <div id="LoadingFileUpload" style="display:none;" ><span class="d-flex align-items-center">
+                                      <span class="spinner-border flex-shrink-0" role="status"></span>
+                                      <span class="flex-grow-1 ms-2">@lang('admin.loading') </span>
+                                  </span> </div>
+                                  <div id="uploadStatus"></div>
+                                  <!--- End Loading --->
+
+                                  <input type="file" name="file" id="fileInput" style="display: flex; color: steelblue; margin-left: 10px; ">
+                                  <div style="display: flex; gap: 10px; margin-bottom: -25px;" ><p>@lang('admin.fileUrl'):</p><p id="filePathUrl"></p></div>
+                                  <button type="button" id="fileUploadClick" class="btn btn-success" style="cursor:pointer; background-image: linear-gradient(#04519b, #033c73 60%, #02325f);color: #ffffff;border-bottom: 1px solid #022241;padding: 12px;border-radius: 6px;display: flex; gap:10px; justify-content: center;align-items: center;">
+                                      <i class="ri-folder-upload-line" style="margin-top: -8px;  margin-bottom: -8px; font-size: 24px;"></i> 
+                                      <p style=" color: blanchedalmond; font-size: 14px; font-weight: bold; " > @lang('admin.fileUpload') </p>
+                                  </button>
+                                  
+                                  <!-- ProgressBar ---->
+                                  <div class="progress" style="margin-top: 14px;height: auto;">
+                                      <div class="progress-bar" id="progressBarFileUpload" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;background-color: teal;color: rgb(255, 255, 255);border-radius: 6px;display: flex;justify-content: center;"></div>
+                                  </div>
+                                  <!-- ProgressBar Son ---->
+                                  
+                              </div>
+                          </form>
+                          <!-- Dosya Yükleme Son ---->
+
+                      </div>
+                      <!-- Dosya Yükleme Kutusu Son ----->
+
+                  </div>
+              </div>
+          </div>
+        </div>
+        
+        <div class="row-fluid">
+          <div class="span6">
+              <div class="control-group">
+                <label class="control-label">Dosya Adı</label>
+                <div class="controls controls-row">
+                  <input type="text" class="input-block-level" name="fileUploadName" id="fileUploadName" placeholder="" value=""  focusType ="true" focusControl="file" focusControl_Active="false" focusOrder="5" >
+                </div>
+              </div>
+          </div>
+          <div class="span6">
+              <div class="control-group">
+                <label class="control-label">İndir</label>
+                <div class="controls controls-row">
+                  <a href="" id="file_download" download="">
+                    <i class="fa fa-download" style="color:#1bb934; font-size: 30px;" ></i>
+                  </a>
+                </div>
+              </div>
+          </div>
+        </div> 
+
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-danger" data-dismiss="modal" aria-hidden="true">@lang('admin.close')</button>
+        <button class="btn btn-info" id="reset_add" >@lang('admin.reset')</button>
+        <button class="btn btn-success" id="new_save_file" >@lang('admin.save')</button>
+      </div>
+  </div>
+  <!----  Modal Dosya Yükleme Son -->
+
   <!--************* Modal Son *********--->
 
 
